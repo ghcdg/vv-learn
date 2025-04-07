81be5569a632a4c15a25b6f7eff6af7d2fcb6e64
ghcdg <2538252675@qq.com>
2025-04-08 01:02:52 +0800

    basic

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,79 @@
 <template>
   <div>
     <p>Current Count In App.vue: {{ count }}</p>
-    <button @click="incrementCount">Increment Count In App.vue</button>  
-    <HelloWorld />
+    <p>todosLength: {{ todosLength }}</p>
+    <p>todosDoneGetter: {{ todosDoneGetter }}</p>
+    <p>countAddNumFunctionGetter: {{ countAddNumFunction(33) }}</p>
+    
+    <p>Options API</p>
+    <p>{{ optIdLengthGetter }}</p>
+    <p>{{ optTodosDoneGetterNewName }}</p>
+
+    <p>自定义函数在 Composition API 语法中实现类似于 mapGetters 的效果</p>
+    <p>{{ optIdLengthGetterHook }}</p>
+    <p>{{ optTodosDoneGetterHook }}</p>
   </div>
 </template>
 
 <script setup>
+// Composition API syntax
 import { useStore } from 'vuex';
 import { computed } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 const storeInstanceInApp = useStore();
-const count = computed(() => storeInstanceInApp.state.count);  
+// 使用 getters 进行复杂逻辑计算, 可以直接通过 store 获取使用
+const todosLength = computed(() => storeInstanceInApp.state.todos.length);
+const todosDoneGetter = computed(() => storeInstanceInApp.getters.todosDoneGetter.length);
 
-const incrementCount = () => {  
-  storeInstanceInApp.commit('increment');  
-};  
+// 利用 getters 创建 computed
+const { count, countAddNumFunctionGetter } = computed(() => {
+  return {
+    count: storeInstanceInApp.getters.idLengthGetter,
+    countAddNumFunctionGetter: storeInstanceInApp.getters.countAddNumFunctionGetter,
+  }
+}).value;
+
+
+const { optIdLengthGetterHook, optTodosDoneGetterHook } = useVuexGetters([
+  'optIdLengthGetter',
+  'optTodosDoneGetter'
+]);
+
+function countAddNumFunction(num) {
+  return countAddNumFunctionGetter(num)
+}
+
+// 自定义函数在 Composition API 语法中实现类似于 mapGetters 的效果
+function useVuexGetters(getterKeys) {
+  const store = useStore();
+  return getterKeys.reduce((acc, key) => {
+    // acc[key] = computed(() => store.getters[key]);
+    acc[key+"Hook"] = computed(() => store.getters[key]+100000);
+    return acc;
+  }, {});
+}
+
+
+</script>
+
+<script>
+// Options API syntax
+import { mapGetters } from 'vuex'
+export default {
+  // ...
+  computed: {
+    // 使用对象展开运算符将 getter 混入 computed 对象中
+    ...mapGetters([
+      'optIdLengthGetter',
+      // ...
+    ]),
+    ...mapGetters({
+      'optTodosDoneGetterNewName': 'optTodosDoneGetter'
+      // ...
+    })
+  }
+}
+
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -5,15 +5,37 @@ import App from './App.vue';
 const store = createStore({
     state () {
         return {
-            count: 0,
+            count: 666,
             id: "is me, the state from main.js",
+            todos: [
+                { id: 1, text: '...', done: true },
+                { id: 2, text: '...', done: false },
+                { id: 3, text: '...', done: true },
+                { id: 4, text: '...', done: false },
+                { id: 5, text: '...', done: true },
+              ],            
         }
     }, 
 
-    mutations: {
-        increment (state) {
-            state.count++;
-        }
+    getters: {
+        todosDoneGetter (state) {
+            return state.todos.filter(todo => todo.done);
+        },
+        idLengthGetter (state) {
+            return state.id.length;
+        },
+        // 让 getters 返回一个函数
+        countAddNumFunctionGetter: (state) => (num) => {
+            return state.count + num;
+        },
+
+        // getters for Options API
+        optTodosDoneGetter (state) {
+            return state.todos.filter(todo => todo.done).length;
+        },
+        optIdLengthGetter (state) {
+            return state.id.length;
+        },
     }
 })
 
